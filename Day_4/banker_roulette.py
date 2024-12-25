abc1d1f56873ce333aa2e869a62b99a92e113050
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2024-12-25 10:08:09 +0100

    20241225.v1008 - created RPS game

--- Day_4/banker_roulette.py
+++ Day_4/banker_roulette.py
@@ -1,3 +1,15 @@
+'''
+This script generates a random friend's name and greets them with a randomly selected greeting.
+
+Extensively uses random.choice() to randomly select an item from a list.
+The function pick_a_friend() returns a randomly selected friend from the list of available friends.
+The function greet_friend() prints a randomly selected greeting along with the friend's name.
+
+To run this script, you can save it in a file named "random_greeting.py" and run it using Python:
+
+This script will output something like:
+Hey, David! You are lucky and can our bills today ��!
+'''
 import random
 
 def pick_a_friend() -> str:
---
+++ Day_4/rock_paper_scissor.py/main.py
@@ -0,0 +1,43 @@
+"""
+Play RPS game in retro mode
+
+uses play_rps function to take user's choice and decides the winner
+"""
+import random
+import rps_ascii
+def play_rps():
+    """
+    Function to play Rock, Paper, Scissors game.
+    Uses user's choice and computer's random choice to determine the winner.
+
+    Args:
+        None
+    """
+    print("Welcome to Rock, Paper, Scissors!")
+
+    user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor \n")
+    user_choice = int(user_input)
+    if user_choice < 0 or user_choice > 2:
+        print("Invalid choice. Please choose again.")
+        return play_rps()
+
+    computer_choice = random.randint(0, 2)
+
+    print("\n##########Game Begins!##########")
+    print(f"You chose: {user_choice}\n\n {rps_ascii.rps_dict[user_choice]}")
+    print(f"Computer chose: {computer_choice}\n\n {rps_ascii.rps_dict[computer_choice]}")
+
+    if (user_choice == 0 and computer_choice == 2 or user_choice == 1 and computer_choice == 0 or user_choice == 2 and computer_choice == 0):
+        print(rps_ascii.win)
+    elif(user_choice == computer_choice):
+        print(rps_ascii.draw)
+    else:
+        print(rps_ascii.loose)
+
+
+if __name__ == "__main__":
+    play_rps()
+
+
+
+
