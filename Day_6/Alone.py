6c710ee8cbd24e1103505cf05341a3f8c0a2e465
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2024-12-27 08:42:18 +0100

    20241227.v0841 - added hurdle #1 challenge

--- Day_6/Alone.py
+++ Day_6/Alone.py
@@ -1,8 +1,17 @@
 """
 https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 
-"""ß
+"""
 
+def square(x):
+    cmove(x)
+    turn_left()
+    cmove(x)
+    turn_left()
+    cmove(x)
+    turn_left()
+    cmove(x)
+    turn_left()
 def turn_right():
     turn_left()    
     turn_left()    
@@ -21,3 +30,21 @@ move()
 # WARNING: Do not change this comment.
 # Library Code is below.
 ################################################################
+# Hurdle #1 challenge
+def turn_right():
+    turn_left()    
+    turn_left()    
+    turn_left()
+    
+def cross_hurdle(x):
+    for _ in range(x):
+        move()
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+        
+cross_hurdle(6)
