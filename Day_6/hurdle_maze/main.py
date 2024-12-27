fbdaaccadaf3e58f91ad1ec5d1b2801fca721611
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2024-12-27 10:57:42 +0100

    20241227.v1057 - added maze project

---
+++ Day_6/hurdle_maze/main.py
@@ -0,0 +1,37 @@
+"""
+ESCAPE THE MAZE
+Write a program using an if/elif/else statement so Reeborg can find the exit. The secret is to have Reeborg follow along the right edge of the maze, turning right if it can, going straight ahead if it can’t turn right, or turning left as a last resort.
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+What you need to know
+The functions move() and turn_left().
+Either the test front_is_clear() or wall_in_front(), right_is_clear() or wall_on_right(), and at_goal().
+How to use a while loop and if/elif/else statements.
+It might be useful to know how to use the negation of a test (not in Python).
+
+"""
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def align_robot_north():
+    while not is_facing_north():
+        turn_left()
+        
+# one time to align the robot north, then start the main loop
+align_robot_north()
+
+while not at_goal():
+    #checks at every move
+    if right_is_clear():
+        turn_right()
+        move()
+    elif not right_is_clear and front_is_clear():
+        move()
+    else:
+        turn_left()    
+        
