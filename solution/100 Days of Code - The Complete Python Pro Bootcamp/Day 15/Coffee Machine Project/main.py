d1c82748afaa44e8c67ee1452faf93406b6ea9a3
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-06 17:02:54 +0100

    20250106.v1502

---
+++ solution/100 Days of Code - The Complete Python Pro Bootcamp/Day 15/Coffee Machine Project/main.py
@@ -0,0 +1,40 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 1. Print report of all coffee resources
+# TODO: 2. Insert coin and process them
+# TODO: 3. Check resources
+# TODO: 4. Ask for user prompt
+# TODO: 5. Make coffee and deduct coffee resources
+# TODO: 6. Declare coffee resources
+# TODO: 7. Declare drink options
