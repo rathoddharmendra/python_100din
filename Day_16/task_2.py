256515d823ce078560a364856c80d10c7645ab05
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-08 09:19:44 +0100

    20250108.v0919 - fixed turtle module on system with added pylance settings

--- Day_16/task_2.py
+++ Day_16/task_2.py
@@ -1,7 +1,7 @@
-from turtle import *
+import turtle as t
 
-t = Turtle( "turtle",1000,True)
-print(f"Is turtle visible: {t.isvisible()}")
+turtle_1= t.Turtle( "turtle",1000,True)
+print(f"Is turtle visible: {turtle_1.isvisible()}")
 # t.forward(25)
 #
 # s = Screen()
---
+++ Day_18/task.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+
+turtle_1 = Turtle("turtle")
+
+
+
+screen = Screen()
+screen.exitonclick()
