2baba15fa5abde4a7c5745470252ad7b42b0897e
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-08 17:08:28 +0100

    20250108.v1708 - added gap functionality to spirograph

--- Day_18/spirograph.py
+++ Day_18/spirograph.py
@@ -1,23 +1,19 @@
 import turtle, random
 from generate_random_color import generate_random_rgb_color
-def draw_next_circles(turtle: turtle.Turtle) -> None:
+def draw_next_circles(turtle: turtle.Turtle, gap: int) -> None:
     counter = 0
-    for degree in range(361):
+    rounds = int(360 / gap)
+    for degree in range(rounds):
         turtle.pencolor(generate_random_rgb_color())
         turtle.circle(100)
-        turtle.left(counter + 1)
-        # turtle.tilt(degree)
-        # turtle.setposition(x=degree, y=degree)
-        # draw spirograph
-
-
+        turtle.setheading(turtle.heading() + gap)
 
 t = turtle.Turtle()
 ts = t.getscreen()
 t.speed("fastest")
 ts.colormode(cmode=255)
 
-draw_next_circles(t)
+draw_next_circles(t, 5)
 
 screen = turtle.Screen()
 screen.exitonclick()
