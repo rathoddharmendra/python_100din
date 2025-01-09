c55c6dd9ffc8ebafd9d869781a92f16db0e3533f
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-09 15:27:43 +0100

    20250109.v1527

--- Day_20 - capstone - snake_game/main.py
+++ Day_20 - capstone - snake_game/main.py
@@ -18,28 +18,35 @@ def make_snake():
 def move_snake():
     pass
 
-for i in range(5):
+for i in range(3):
     new_snake = Turtle(shape="square")
     new_snake.color(generate_random_rgb_color())
     # new_snake.color('white')
     new_snake.penup()
     x_position = new_snake.xcor() - (i * 20)
     new_snake.goto(x=x_position, y=0)
-
+    new_snake.speed(0)
     snakes.append(new_snake)
 
 # move the snake
 is_game = True
-possible_directions = [0,90, 180, 270]
+possible_directions = [90, 270]
+
+# listen for keypresses
 while is_game:
     screen.update()
     time.sleep(0.1)
-    direction = random.choice(possible_directions) # set by user's keystroke
-
-    for snake_index in range(1, len(snakes) + 1):
-        snake = snakes[len(snakes) - snake_index]
-        snake.setheading(snake.heading() + direction)
-        snake.forward(10)
+   
+    # make the body follow first snake
+    for snake_index in range(len(snakes) -1, 0, -1): # 2
+        x_pos = snakes[snake_index - 1].xcor()
+        y_pos = snakes[snake_index - 1].ycor()
+        snakes[snake_index].goto(x_pos, y_pos)
+        screen.update()
+    # direction = random.choice(possible_directions) # set by user's keystroke
+    snakes[0].forward(20)
+    snakes[0].left(random.choice(possible_directions))
+    
     # check for collision with border
 
 screen.listen()
