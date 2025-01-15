7f6aac14614a658b3f18485d79ec1c273c865725
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-15 17:02:32 +0100

    20250115.v1702 - added counter to pomodoro

--- Day_28 - pomodoro/pomodoro-start/main.py
+++ Day_28 - pomodoro/pomodoro-start/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import os
+import os, time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,14 +11,35 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 30
-
+tick_mark = '✅'
+    
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def resetTimer():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def startTimer():
+    countdown(text_id=canvas_text, secs=60, min=WORK_MIN - 1)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+# freq: secs
+def countdown(text_id: int, min: int, secs: int):
+    if secs <= 0:
+        min -= 1
+        secs = 60
+    if min < 0:
+        update_canvas_text(text_id, f"DONE")
+        return # end the countdown
+        # todo - add a sound
+        # todo - add a break point with reset click
+    secs -= 1
+    update_canvas_text(text_id, f"{min:02d}:{secs:02d}")
+    canvas.after(1000,countdown,text_id, min, secs)
+    # todo - set break point with reset click
+
+# Update the text
+def update_canvas_text(text_id: int, update_text: str):
+    canvas.itemconfig(text_id, text=update_text)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -26,29 +47,63 @@ window.title("Pomodoro Timer")
 window.minsize(width=250, height=250)
 window.config(padx=25, pady=25, bg=YELLOW)
 
+header = Label(window, text="Timer", font=(FONT_NAME, 36), fg=GREEN, bg=YELLOW)
+header.grid(row=0, column=1)
+
 # create background tomato and put a timer widget on top of it
 # new_learnen - canvas for background
-canvas = Canvas(window, width=200, height=250) # bg=transparent
+canvas = Canvas(window, width=220, height=250, bg=YELLOW, highlightthickness=0) # bg=transparent
 bg_image = PhotoImage(file=os.path.join(os.path.dirname(__file__), "tomato.png"))
 canvas.create_image(103, 125,image=bg_image)
-h = 00
-m = 0
-s = 0
-canvas.create_text(100,140,text=f'0{h}:0{m:0.2f}', font=(FONT_NAME, 24, 'bold'), fill=YELLOW )
+
+canvas_text = canvas.create_text(100,140,text=f'25:00', font=(FONT_NAME, 28, 'bold'), fill=YELLOW )
 canvas.grid(row=1, column=1)
+
+print(canvas_text)
 
 
 # create buttons - start and reset timer
-def startTimer():
-    pass
-start = Button(window, text="Start", command=startTimer)
+
+start = Button(
+    window, 
+    text="Start", 
+    command=startTimer, 
+    fg=GREEN, 
+    highlightbackground=YELLOW, 
+    font=(FONT_NAME, 16, 'bold'), 
+    highlightthickness=0,
+    padx=10,
+    pady=10, 
+    )
 start.grid(row=2, column=0)
 
-def resetTimer():
-    pass
 
-reset = Button(window, text="Reset", command=resetTimer)
+
+reset = Button(
+    window,
+    text="Reset", 
+    command=resetTimer, 
+    fg=PINK, 
+    highlightbackground=YELLOW, 
+    font=(FONT_NAME, 16, 'bold'), 
+    highlightthickness=0,
+    padx=10,
+    pady=10, 
+    )
 reset.grid(row=2, column=2)
+
+# create labels for work, short and long breaks
+
+
+# create checkmarks for each completed break
+check_mark_name = Label(
+        window,
+        text=tick_mark + "   " + tick_mark+ "   " + tick_mark+ "   " + tick_mark,
+        bg=YELLOW,
+        highlightbackground=YELLOW,
+        highlightthickness=0,
+        )
+check_mark_name.grid(row=3, column= 1)
 
 window.mainloop()
 
