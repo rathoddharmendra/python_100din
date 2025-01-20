c0af880316b8680f9b30f96af6845d1246b43ee3
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-20 17:17:16 +0100

    20250120.v1717 - completed rain alert

--- Day_35 - rain alert/main.py
+++ Day_35 - rain alert/main.py
@@ -1,14 +1,16 @@
 import requests
 import pandas, json, os
+from sms import SmsClient
 
+sms = SmsClient()
 # https://api.openweathermap.org/data/2.5/weather?q=Berlin,DE&appid=cf142a6e3d55102c8a186cd7c70f0819
-API_KEY = 'cf142a6e3d55102c8a186cd7c70f0819'
+OPEN_WEATHER_MAP_API_KEY = os.environ.get('OPEN_WEATHER_MAP_API_KEY')
 BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 # Replace with your own API key
 parameters = {
     'id':2950159,
-    'appid':API_KEY,
+    'appid':OPEN_WEATHER_MAP_API_KEY,
     'cnt': 8,
     'lat': -15.793889, 
     'lon': -47.882778
@@ -25,8 +27,9 @@ with open(os.path.join(os.path.dirname(__file__),'data.json'), mode='w') as file
 is_rain = False
 for weather in data['list']:
     if weather['weather'][0]['id'] < 700:
-        is_rain = True
         # print(f'Advised to bring an umbrella ☔️ - for there might be {weather['weather'][0]['description']} at {weather['dt_txt']}')
+        is_rain = True
 
 if is_rain:
     print('Advised to carry an umbrella today ☔️')
+    sms.send_sms('Advised to carry an umbrella today ☔️\n-By Dee')
