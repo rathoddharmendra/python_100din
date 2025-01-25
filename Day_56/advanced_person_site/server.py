71b7000e6113c580bd5c442b142b6b8cdff67004
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-25 15:12:49 +0100

    20250125.v1512 - added cv project

--- Day_56/advanced_person_site/server.py
+++ Day_56/advanced_person_site/server.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Dee", age=30)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
