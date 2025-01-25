2cacf299534a5f0fd31c628e80c79a43e4703255
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-25 14:56:44 +0100

    20250125.v1456

--- Day_56/my_personal_site/main.py
+++ Day_56/my_personal_site/main.py
@@ -6,9 +6,18 @@ index_page_path =  os.path.join(os.path.dirname(__file__),'../pages/index.html')
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def main():
     return render_template(template_name_or_list='index.html',  
                            message='I\'m Dee')
+
+
+@app.route("/resume")
+def resume():
+    return render_template(template_name_or_list='resume.html')
+
+@app.route("/cv")
+def cv():
+    return render_template(template_name_or_list='cv.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
