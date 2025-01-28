e9982c76df991025e4cd644657ecd6fe745f1cf4
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-28 16:04:38 +0100

    20250128.v1604 - added contact form

---
+++ Day_60 - contact form/new-form/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def submit():
+    print(request.form['name'])
+    return f'<h1> Form submitted successfully by {request.form['name']}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
