db2e7f0ec7159c60a6ffe1925a8e6fccd3902fb1
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-28 13:09:54 +0100

    20250128.v1309 - final templating d_57 project

--- Day_57 - Jinja/Final - starting-files-blog-templating/main.py
+++ Day_57 - Jinja/Final - starting-files-blog-templating/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
-
+API_URL = 'https://api.npoint.io/c790b4d5cab58020d391'
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(API_URL).json()
+    # Your code here to iterate over the data and extract relevant information
 
+    return render_template("index.html", blogs=response)
+
+
+@app.route('/blog/<int:blog_id>')
+def get_blog(blog_id: int):
+    response = requests.get(API_URL).json()[blog_id - 1]
+    # Your code here to iterate over the data and extract relevant information
+    return render_template("post.html", blog=response)
 if __name__ == "__main__":
     app.run(debug=True)
