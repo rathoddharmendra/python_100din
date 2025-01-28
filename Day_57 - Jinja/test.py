fbe2fd18e175aa2678f69d9df7cd3a8d49b55c44
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-28 12:51:43 +0100

    20250128.v1251

---
+++ Day_57 - Jinja/test.py
@@ -0,0 +1,21 @@
+from flask import url_for, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<string:username>')
+def profile(username:str):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='samurai', path='login'))
+    print(url_for('profile', username='John Doe'))
