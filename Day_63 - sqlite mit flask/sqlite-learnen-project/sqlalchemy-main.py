ad976283b71ae015c62eb6b09111f7cb0c0e5120
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-29 15:12:39 +0100

    20250129.1512 - fixed python dependency

--- Day_63 - sqlite mit flask/sqlite-learnen-project/sqlalchemy-main.py
+++ Day_63 - sqlite mit flask/sqlite-learnen-project/sqlalchemy-main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template #, request , redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
@@ -7,3 +7,18 @@ import os
 db_path = os.path.join(os.path.dirname(__file__), 'new-books-collection.db')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+class Books(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
