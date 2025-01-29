c71d00320d6a3f8a978c624f0b1a5cf77aac2086
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-29 16:03:45 +0100

    20250129.v1603 - added sqlalchemy learning code

--- Day_63 - sqlite mit flask/sqlite-learnen-project/sqlalchemy-main.py
+++ Day_63 - sqlite mit flask/sqlite-learnen-project/sqlalchemy-main.py
@@ -19,6 +19,40 @@ class Books(db.Model):
     author: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
 
+    # def __repr__(self) -> str:
+    #     return f'<Book {self.title}>'
+
 with app.app_context():
     db.create_all()
+
+# with app.app_context():
+#     book = Books(title='Clean New Code', author='Caro C. Jeffrey', rating=1.2)
+#     db.session.add(book)
+#     db.session.commit()
+
+# Reads all Books from the database
+with app.app_context():
+    books = db.session.execute(db.select(Books).order_by(Books.title)).scalars().all()
+    print([book.title for book in books])
+
+# Read one row
+with app.app_context():
+    book = db.session.execute(db.select(Books).where(Books.title == "New Code")).scalar()
+    print(f'{book=}')
+
+# Update a book in the database
+# with app.app_context():
+#     book_id = 1
+#     book_to_update = db.get_or_404(Books, book_id)
+#     book_to_update.title = 'New Code - updated'
+# # OR find using where clause, 
+#     book = db.session.execute(db.select(Books).where(Books.title == "New Code")).scalar()
+#     book.title = 'New Code - updated'
+#     db.session.commit()
+# delete a book from the database
+# with app.app_context():
+#     book_id = 1
+#     book_to_delete = db.get_or_404(Books, book_id)
+#     db.session.delete(book_to_delete)
+#     db.session.commit()
 
