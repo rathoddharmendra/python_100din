6653b3d66d97c9758a758c8c10297f29ec201408
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-29 10:28:45 +0100

    20250129.v1028

--- Day_61 - WTF form validators/flask_secrets/main.py
+++ Day_61 - WTF form validators/flask_secrets/main.py
@@ -1,7 +1,8 @@
+# type: ignore
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, input_required, ValidationError
+from wtforms.validators import DataRequired, input_required, ValidationError, Email, Length
 from flask_wtf.csrf import CSRFProtect
 from wtforms.fields import SelectField, SubmitField
 from wtforms.widgets import SubmitInput
@@ -24,6 +25,11 @@ SECRET_KEY = 'my super secret key'.encode('utf8')
 app.secret_key = SECRET_KEY
 csrf = CSRFProtect(app)
 
+
+username = 'admin@gmail.com'
+
+user_password = 12345678
+
 WTF_CSRF_SECRET_KEY = 'a random string'
 
 class MyForm(FlaskForm):
@@ -32,9 +38,9 @@ class MyForm(FlaskForm):
         locales = ('en_US', 'en')
 
     name = StringField(label='Name', validators=[DataRequired(), input_required()], name='name')
-    email = StringField(label='Email', validators=[DataRequired()], name='email', description='We will never share your email with anyone else.')
-    password = PasswordField(label='Password', validators=[DataRequired()], name='password')
-    select = SelectField(label='Select', choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three')], validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(message='That\'s not a valid email address.')], name='email', description='We will never share your email with anyone else.')
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=3, max=15, message='Password must be between 3 and 15 letters')], name='password')
+    select = SelectField(label='Select your choice: ', choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three')], validators=[DataRequired()])
     accept_rules = BooleanField('I accept the site rules', [input_required()])
     submit = SubmitField(label='Submit')
     # go = SubmitInput('Go')
@@ -48,20 +54,28 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     my_form = MyForm()
+    if request.method == 'POST':
+        if my_form.validate_on_submit():
+            if my_form.password == user_password and my_form.email == username:
+                return render_template('success.html')
+            else:
+                return render_template('denied.html')
+    print(my_form.data.items())
+    # my_form.validate()
     return render_template('login.html', form=my_form)
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
-    my_form = MyForm()
-    if my_form.validate_on_submit():
-        print('Form validated')
-        print(my_form.data.items())
-        return render_template('success.html')
+# @app.route('/submit', methods=['GET', 'POST'])
+# def submit():
+#     my_form = MyForm()
+#     if my_form.validate_on_submit():
+#         print('Form validated')
+#         print(my_form.data.items())
+#         return render_template('success.html')
     
-    print('Form not validated')
-    print(my_form.data.items())
+#     print('Form not validated')
+#     print(my_form.data.items())
 
-    return render_template('denied.html')
+#     return render_template('denied.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
