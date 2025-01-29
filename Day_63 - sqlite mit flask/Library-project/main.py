664f401e30d01a5b7bbf1d4114242df63dc2e6f2
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-29 17:10:02 +0100

    20250129.v1709 - complete Library project

--- Day_63 - sqlite mit flask/Library-project/main.py
+++ Day_63 - sqlite mit flask/Library-project/main.py
@@ -3,25 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
-from flask_bootstrap import Bootstrap4
+# from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired
 import os
 
-db_path = os.path.join(os.path.dirname(__file__), 'new-books-collection.db')
+db_path = os.path.join(os.path.dirname(__file__), 'books-collection.db')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-bootstrap = Bootstrap4(app)
+# bootstrap = Bootstrap4(app)
 
 
 class Book(FlaskForm):
     id = IntegerField('ID', validators=[DataRequired()])
     title = StringField('Book Title', validators=[DataRequired()])
     author = StringField('Author Name', validators=[DataRequired()])
-    Rating = StringField('Ratings', validators=[DataRequired()])
+    rating = StringField('Ratings', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -32,8 +32,8 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 class Books(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    title: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
-    author: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(30), nullable=False)
+    author: Mapped[str] = mapped_column(String(30), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
 
     # def __repr__(self) -> str:
@@ -49,6 +49,8 @@ def update_books():
 
 all_books = update_books()
 
+
+# view functions below
 @app.route('/')
 def home():
     print(all_books)
@@ -69,25 +71,51 @@ def add():
         return redirect(url_for('home'))
     return render_template("add.html")
 
+# simple version - allows rating to be updated
+# @app.route("/edit/<int:book_id>/<string:title>/<string:author>/<float:rating>", methods=["GET", "POST"])
 @app.route("/edit/<int:book_id>", methods=["GET", "POST"])
-def edit(book_id: int):
+def edit(book_id: int = -1):
     global all_books
-    form = Book()
+    if book_id == -1 :
+        book_id = int(request.form['id'])
     if request.method == 'POST':
         with app.app_context():
             book_to_update = db.get_or_404(Books, book_id)
-            book_to_update.title = request.form['title']
-            book_to_update.author = request.form['author']
             book_to_update.rating = request.form['rating']
             db.session.commit()
 
         all_books = update_books()
         return redirect(url_for('home'))
-    return render_template("edit.html", form=form)
+    with app.app_context():
+        # book = db.get_or_404(Books, book_id).scalar()
+        book = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+        title = book.title
+        author = book.author
+        rating = book.rating
+    return render_template("edit.html", book={'id': book_id, 'title': title, 'author': author, 'rating': rating})
+
+# @app.route('/update', methods=["POST"])
+# def update():
+#     return edit()
+# @app.route("/edit/<int:book_id>", methods=["GET", "POST"])
+# def edit(book_id: int):
+#     global all_books
+#     form = Book()
+#     if request.method == 'POST':
+#         with app.app_context():
+#             book_to_update = db.get_or_404(Books, book_id)
+#             book_to_update.title = request.form['title']
+#             book_to_update.author = request.form['author']
+#             book_to_update.rating = request.form['rating']
+#             db.session.commit()
+
+#         all_books = update_books()
+#         return redirect(url_for('home'))
+#     return render_template("edit.html", form=form)
     
 
 @app.route("/delete/<int:book_id>")
-def delete(book_id):
+def delete(book_id: int):
     global all_books
     with app.app_context():
         book_to_delete = db.get_or_404(Books, book_id)
---
+++ Day_63 - sqlite mit flask/readme.md
@@ -0,0 +1,19 @@
+      <!-- <div class="container">
+        <form action="{{ url_for('edit')}}" method="POST">
+          {{ form.hidden_tag() }}
+          <div class="form-group">
+            {{ form.title.label }}
+            {{ form.title(class="form-control") }}
+          </div>
+          <div class="form-group">
+            {{ form.author.label }}
+            {{ form.author(class="form-control") }}
+          </div>
+          <div class="form-group">
+            {{ form.rating.label }}
+            {{ form.rating(class="form-control") }}
+          </div>
+          <div class="form-group">
+            {{ form.submit(class="form-control") }}
+        </form>
+      </div> -->
