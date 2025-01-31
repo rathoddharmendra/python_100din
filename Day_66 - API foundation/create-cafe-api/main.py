93bbbe3d8fd042bbd9ae3d3bc6ea39acf3540694
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-31 10:36:32 +0100

    20250131.v1036 - created post api endpoints

--- Day_66 - API foundation/create-cafe-api/main.py
+++ Day_66 - API foundation/create-cafe-api/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 import os, random
+from typing import Dict, Any
 # from flask.json import jsonify
 
 
@@ -44,7 +45,6 @@ def home():
 
 
 # HTTP GET - Read Record
-@app.route('/all', methods=['GET'])
 @app.route('/random', methods=['GET'])
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
@@ -71,13 +71,88 @@ def get_random_cafe():
 
     # cafe = Cafe.query.order_by(db.func.random()).first()
     # return jsonify(cafe)
+@app.route('/all', methods=['GET'])
+def get_all_cafes():
+    cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    response = { 'cafes': [] }
+    for cafe in cafes:
+        response['cafes'].append({cafe.id : {
+            'name': cafe.name,
+            'map_url': cafe.map_url,
+            'img_url': cafe.img_url,
+            'location': cafe.location,
+            'seats': cafe.seats,
+            'has_toilet': cafe.has_toilet,
+            'has_wifi': cafe.has_wifi,
+            'has_sockets': cafe.has_sockets,
+            'can_take_calls': cafe.can_take_calls,
+            'coffee_price': cafe.coffee_price,
+        }})
+    return jsonify(response)
+
+@app.route('/search', methods=['GET'])
+def search_cafe():
+    location = request.args.get('loc').title()
+    # location = request.form.get('loc')
+    # if location is not None:
+    #     location = location.title()
+    cafes = db.session.execute(db.select(Cafe).where(Cafe.location == location)).scalars().all()
+    if cafes:
+        response = { 'cafes': [] }
+        for cafe in cafes:
+            response['cafes'].append({
+                'id': cafe.id,
+                'name': cafe.name,
+                'map_url': cafe.map_url,
+                'img_url': cafe.img_url,
+                'location': cafe.location,
+                'seats': cafe.seats,
+                'has_toilet': cafe.has_toilet,
+                'has_wifi': cafe.has_wifi,
+                'has_sockets': cafe.has_sockets,
+                'can_take_calls': cafe.can_take_calls,
+                'coffee_price': cafe.coffee_price,
+            })
+        return jsonify(response)
+    return jsonify({'error': 'Cafe not found'})
 
 # HTTP POST - Create Record
+@app.route('/add', methods=['POST'])
+def add_cafe():
+    new_cafe = Cafe(
+        name = request.form.get('name'),
+        map_url=request.form.get('map_url'),
+        img_url=request.form.get('img_url'),
+        location=request.form.get('location'),
+        has_toilet = bool(request.form.get('has_toilet')),
+        has_wifi = bool(request.form.get('has_wifi')),
+        has_sockets = bool(request.form.get('has_sockets')),
+        can_take_calls = bool(request.form.get('can_take_calls')),
+        seats = request.form.get('seats'),
+        coffee_price = request.form.get('coffee_price')
+        )
+    print(request.form.to_dict())
+    try:
+        db.session.add(new_cafe)
+        db.session.commit()
+
+        return jsonify({'success': 'Cafe added successfully'})
+    except Exception as e:
+        return jsonify({'error': f'Error adding cafe with error {e}'})
 
 # HTTP PUT/PATCH - Update Record
+@app.route('/update-price/<int:cafe_id>', methods=['PATCH'])
+def update_price(cafe_id: int):
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = request.args.get('new_price')        
+        db.session.commit()
+        return jsonify({'success': 'Price updated successfully'})
+    return jsonify({'error': 'Cafe not found'})
 
 # HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True) -- cannot run vscode debug mode with error "No module named main"
+    app.run()
