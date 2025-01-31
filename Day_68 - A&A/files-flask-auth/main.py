d58d54639cd63ed08958e52f6d3344c055672835
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-01-31 21:07:41 +0100

    20250131.v0907

--- Day_68 - A&A/files-flask-auth/main.py
+++ Day_68 - A&A/files-flask-auth/main.py
@@ -4,23 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 # CREATE DATABASE
-
+db_path = os.path.join(os.path.dirname(__file__), 'instance/users.db')
 
 class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 # CREATE TABLE IN DB
-
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -38,8 +38,27 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Hashing and salting the password entered by the user 
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        # Storing the hashed password in our database
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=hash_and_salted_password,
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("secrets.html", name=request.form.get('name'))
+
     return render_template("register.html")
 
 
