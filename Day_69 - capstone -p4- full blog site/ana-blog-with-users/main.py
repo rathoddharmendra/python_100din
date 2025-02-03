5852c5e9d450d71d23add9de90ee1832cae1a99a
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-02-03 10:25:23 +0100

    20250203.v1025

--- Day_69 - capstone -p4- full blog site/ana-blog-with-users/main.py
+++ Day_69 - capstone -p4- full blog site/ana-blog-with-users/main.py
@@ -19,8 +19,14 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
-# TODO: Configure Flask-Login
 
+# TODO: Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
