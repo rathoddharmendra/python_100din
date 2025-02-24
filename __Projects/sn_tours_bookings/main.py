2cea9403d7992d94e847b9d4255eeeaa1be91b59
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-02-24 08:54:43 +0100

    20250224.v0854 - add email sending functionality and SQLite database integration

--- __Projects/sn_tours_bookings/main.py
+++ __Projects/sn_tours_bookings/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+from conn import Connection
+from db import generate_cursor
+
 
 app = Flask(__name__)
+
+email = Connection()
+db_cursor, db_conn = generate_cursor()
 
 @app.route('/')
 def home():
@@ -10,9 +16,14 @@ def home():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        print('form submitted')
-        print(request.form.get('name'))
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+
         # send an email here
+
     return render_template('contact.html')
 
 # @app.route('/post/<int:blog_id>')
