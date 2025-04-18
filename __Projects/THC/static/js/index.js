18b9a14eccb4754a42d813f7e7d129b2539384c9
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-04-18 15:17:13 +0200

    feat: add booking system with database integration; implement contact form and navigation; create header, footer, and booking templates

---
+++ __Projects/THC/static/js/index.js
@@ -0,0 +1,42 @@
+let isNavOpen = false;
+
+    function toggleNav(){
+        if (!isNavOpen){
+            openNav()
+        } 
+        else {
+            closeNav()
+        }
+        isNavOpen = !isNavOpen;
+    }
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
