0ad85b29ce526ef9396f7759f2cae8f4ecc65770
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-04-19 13:46:17 +0200

    feat: update templates and styles for The Hikers Club; add event creation and display functionality; enhance README with task list

---
+++ __Projects/THC/main.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request
+from db import generate_cursor
+import random, os
+
+app = Flask(__name__)
+
+CITIES = ['Berlin', 'Bangalore', 'San Francisco', 'New York', 'London', 'Paris', 'Tokyo']
+COUNTRIES = ['Germany', 'India', 'USA', 'UK', 'France', 'Japan']
+
+city = random.choice(CITIES)
+filepath = os.path.join(os.path.dirname(__file__), 'events.db')
+
+country = random.choice(COUNTRIES)
+app.config['CITY'] = city
+app.config['COUNTRY'] = country
+app.config['DATABASE'] = f'sqlite:///{filepath}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE']
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True,
+    'pool_recycle': 1800,
+    'pool_size': 20,
+    'max_overflow': 0
+}
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 1800
+app.config['SQLALCHEMY_POOL_SIZE'] = 20
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
+app.config['SQLALCHEMY_POOL_PRE_PING'] = True
+
+all_events = [
+    {
+        'id': 1,
+        'title': 'Event 1',
+        'name': 'Event 1',
+        'description': 'Description for Event 1',
+        'date': '2023-10-01',
+        'location': 'berlin'
+    },
+    {
+        'id': 2,
+        'title': 'Event 2',
+        'name': 'Event 2',
+        'description': 'Description for Event 2',
+        'date': '2023-10-02',
+        'location': 'berlin'
+
+    },
+    {
+        'id': 3,
+        'title': 'Event 3',
+        'name': 'Event 3',
+        'description': 'Description for Event 3',
+        'date': '2023-10-03',
+        'location': 'berlin'
+
+    },
+    {
+        'id': 1,
+        'title': 'Event 1',
+        'name': 'Event 1',
+        'description': 'Description for Event 1',
+        'date': '2023-10-01',
+        'location': 'berlin'
+
+    },
+    {
+        'id': 2,
+        'title': 'Event 2',
+        'name': 'Event 2',
+        'description': 'Description for Event 2',
+        'date': '2023-10-02',
+        'location': 'berlin'
+
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', events=all_events[:3])
+
+@app.route('/create-event', methods=['GET', 'POST'])
+def create_event():
+    if request.method == 'POST':
+        event_id = request.form.get('event_id')
+        # event = next((event for event in all_events if event['id'] == int(event_id)), None)
+        return render_template('create-event.html')
+    return render_template('create-event.html')
+
+@app.route('/all-events', methods=['GET', 'POST'])
+def show_events():
+    return render_template('show-events.html', events=all_events)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000, host='0.0.0.0')
+    
---
+++ __Projects/THC/readme.md
@@ -0,0 +1,16 @@
+todo:
+
+1. main page
+   [x] launch initial page
+   [x] show events on page
+2. create event page experience  - save in DB .. and show on page todo 5
+3. create event detail page  - design
+4. Add join now functionality on detail page, and on card
+5. Show events on page
+
+5. create global variable for city - and integrate in landing page and all events page
+   1. values - global, Berlin, Bangalore, etc
+Notes - 
+    <script src="https://kit.fontawesome.com/5bde470069.js" crossorigin="anonymous"></script>
+ correct fontawesome link
+
