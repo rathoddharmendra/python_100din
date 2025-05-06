889a0b518fb869d55af338d7be0a32126bed5f7a
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-05-06 11:58:21 +0200

    feat: enhance Redis message handling; modify message sending order, add JSON serialization, and implement list and hash operations

--- __Projects/Data_Platform/redis/main.py
+++ __Projects/Data_Platform/redis/main.py
@@ -1,20 +1,20 @@
 from redis import Redis
 import time
+import json
 
 # r = Redis(host='redis-jmwy.sliplane.app', port=6379, password='Q2edJucKQdYtkk5O')
 r = Redis(host='localhost', port=6379, password='root')
 print(f'Connection status to Redis server: {r.ping()}')  # Should return True
-
 
 # lets run a loop:
 
 
 def send_message():
     for i in range(100):
-        r.set(f'message_{i}', f'This is message number {i}')
+        r.set(f'message_{i}', f'This is message number {100 - i}')
         print(f'Sent message: {i}')
         # sleep for 1 second
-        time.sleep(1)
+        # time.sleep(1)
     print('All messages sent!')
 
 def get_message():
@@ -22,7 +22,7 @@ def get_message():
         message = r.get(f'message_{i}')
         print(f'Received message: {message.decode("utf-8")}')
         # sleep for 1 second
-        time.sleep(1)
+        # time.sleep(1)
     print('All messages received!')
 
 
@@ -31,3 +31,19 @@ if __name__ == '__main__':
 
     get_message()
 
+data = {'name': 'Alice', 'age': 30}
+r.set('user:1001', json.dumps(data))
+
+# Retrieve and parse
+user_data = json.loads(r.get('user:1001'))
+print(user_data['name'])  # Alice
+
+
+# List
+r.rpush('tasks', 'task1')
+r.rpush('tasks', 'task2')
+print(r.lrange('tasks', 0, -1))  # [b'task1', b'task2']
+
+# Hash
+r.hset('user:1', mapping={'name': 'John', 'age': 28})
+print(r.hgetall('user:1'))  # {b'name': b'John', b'age': b'28'}
